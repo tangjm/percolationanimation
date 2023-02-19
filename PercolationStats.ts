7480a20fb66209472b7308ea7b009c1547f0cb03
tangjm <tangjm.dev@outlook.com>
2023-02-19 22:03:20 +0000

    Remove comments in emitted Javascript

--- PercolationStats.ts
+++ PercolationStats.ts
@@ -1,4 +1,5 @@
 import PercolationGrid from "./PercolationGrid.js";
+import { UserOptions } from "./types.js";
 
 export default interface PercolationStats {
   trials: {
@@ -13,4 +14,89 @@ export default interface PercolationStats {
   updateMean: () => void;
   updateStdDev: () => void;
   updateResults: (g: PercolationGrid) => void;
+  updateDOM: (s: string, val: any) => void;
+}
+
+export class MonteCarloSimulation implements PercolationStats {
+  trials: {
+    total: number;
+    completed: number;
+  };
+  percolationThresholds: number[];
+  mean: number | null;
+  stdDev: number | null;
+  updateDOM: (s: string, val: number) => void;
+
+  constructor(
+    config: UserOptions,
+    updateDOM: (s: string, val: number) => void
+  ) {
+    this.trials = {
+      total: config.getNumOfGrids(),
+      completed: 0,
+    };
+    this.percolationThresholds = [];
+    this.mean = null;
+    this.stdDev = null;
+    this.updateDOM = updateDOM;
+    updateDOM("trialsTotal", this.trials.total);
+  }
+  incrementTrialsCompleted() {
+    this.trials.completed++;
+  }
+  addPercolationThreshold(percolationGrid: PercolationGrid) {
+    const percolationThreshold =
+      percolationGrid.percolation.numberOfOpenSites() /
+      Math.pow(percolationGrid.percolation.n, 2);
+    this.percolationThresholds.push(percolationThreshold);
+  }
+  updateMean() {
+    this.mean =
+      this.percolationThresholds.reduce((a: number, b: number) => a + b, 0) /
+      this.trials.completed;
+  }
+  updateStdDev() {
+    const sumVariances = this.percolationThresholds.reduce(
+      (acc: number, threshold: number) => {
+        const diff = threshold - this.mean;
+        return acc + Math.pow(diff, 2);
+      },
+      0
+    );
+    this.stdDev = sumVariances / this.trials.completed;
+  }
+  updateResults(percolationGrid: PercolationGrid) {
+    this.incrementTrialsCompleted();
+    this.addPercolationThreshold(percolationGrid);
+    this.updateMean();
+    this.updateStdDev();
+    this.updateDOM("trialsCompleted", this.trials.completed);
+    this.updateDOM("mean", this.mean);
+    this.updateDOM("stdDev", this.stdDev);
+  }
+}
+
+export function updateDOM(e: string, val: any): void {
+  function updateTextContent(e: Element, text: string) {
+    e.textContent = text;
+  }
+  function updateLastElementChild(e: Element, text: string) {
+    let lastChild = e.lastElementChild;
+    if (!lastChild) return;
+    lastChild.textContent = text;
+  }
+  const elements = {
+    trialsTotal: document.querySelector("b[id='trials-total']"),
+    trialsCompleted: document.querySelector("b[id='trials-completed']"),
+    mean: document.querySelector("p[id='mean']"),
+    stdDev: document.querySelector("p[id='stdDev']"),
+  };
+  const table = {
+    trialsTotal: updateTextContent,
+    trialsCompleted: updateTextContent,
+    trials: updateLastElementChild,
+    mean: updateLastElementChild,
+    stdDev: updateLastElementChild,
+  };
+  return table[e](elements[e], String(val));
 }
